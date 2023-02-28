8b27b67ff24600ee66f60f9427f36666ce2f36fe
kaibalya jena <kaibalya18j@gmail.com>
2023-03-01 02:41:01 +0530

    making components and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import {Route,Routes} from 'react-router-dom'
+import { CampaignDetails,CreateCampaign,Home,Profile } from './pages'
+import { Navbar,Sidebar } from './components'
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row'>
+      <div className='sm:flex hidden mr-10 relative'>
+        <Sidebar />
+      </div>
+      <p className='font-xl font-bold'>testing tailwing css</p>
+      <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home/>} />
+        </Routes>
+      </div>
+    </div>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -21,6 +21,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from 'react-router-dom';
 import {ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import './index.css'
 
 // This is the chain your dApp will work on.
 // Change this to the chain your app is built for.
