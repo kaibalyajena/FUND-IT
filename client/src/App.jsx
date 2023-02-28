a7ff87e197c4bd4684f92b2e94d43c1f548131f2
kaibalya jena <kaibalya18j@gmail.com>
2023-03-01 02:48:33 +0530

    sidebar done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,6 @@ const App = () => {
       <div className='sm:flex hidden mr-10 relative'>
         <Sidebar />
       </div>
-      <p className='font-xl font-bold'>testing tailwing css</p>
       <div className='flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5'>
         <Navbar />
         <Routes>
