1a55af84afac8af684ec22b7b24552fb75b32679
kaibalya jena <kaibalya18j@gmail.com>
2023-03-27 22:26:57 +0530

    campaign details page

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -8,6 +8,7 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
   
   return (
     <div className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer" onClick={handleClick}>
+    {/* <div onClick={handleClick} className="sm:w-[288px] h-full w-full bg-gray-500 rounded-[15px] bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-50"> */}
       <img src={image} alt="fund" className="w-full h-[158px] object-cover rounded-[15px]"/>
 
       <div className="flex flex-col p-4">
